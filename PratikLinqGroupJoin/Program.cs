d10de4ef5d72adc9e0d82742c0fe5fd0e8368f19
izzetcosar <izzetcosar@gmail.com>
2024-11-25 23:49:45 +0300

    PratikLinqGroupJoin

---
+++ PratikLinqGroupJoin/Program.cs
@@ -0,0 +1,52 @@
+﻿using PratikLinqGroupJoin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<Student> students = new List<Student>();
+        
+students.Add(new Student { StudentId = 1, StudentName = "Ali", ClassId = 1 });
+students.Add(new Student { StudentId = 2, StudentName = "Ayşe", ClassId = 2 });
+students.Add(new Student { StudentId = 3, StudentName = "Mehmet", ClassId = 1 });
+students.Add(new Student { StudentId = 4, StudentName = "Fatma", ClassId = 3 });
+students.Add(new Student { StudentId = 5, StudentName = "Ahmet" , ClassId = 2 });
+students.Add(new Student { StudentId = 6, StudentName = "İzzet", ClassId = 1 });
+students.Add(new Student { StudentId = 7, StudentName = "Kemal", ClassId = 3 });
+students.Add(new Student { StudentId = 8, StudentName = "Hamit", ClassId = 2 });
+students.Add(new Student { StudentId = 9, StudentName = "Buket", ClassId = 2 });
+students.Add(new Student { StudentId = 9, StudentName = "Nurten", ClassId = 3 });
+
+List<Class> classes = new List<Class>();
+
+classes.Add(new Class { ClassId = 1, ClassName = "Matematik" });
+classes.Add(new Class { ClassId = 2, ClassName = "Türkçe" });
+classes.Add(new Class { ClassId = 3, ClassName = "Kimya" });
+
+var studentWithClass = classes.GroupJoin(
+            students, // Eşleştirilecek ikinci liste
+            className => className.ClassId, // classes için anahtar
+            student => student.ClassId,  // students için anahtar
+            (className, studentGroup) => new // Her eşleşmede oluşturulacak yapı
+            {
+                ClassName = className.ClassName,
+                Students = studentGroup
+            });
+foreach (var group in studentWithClass)
+{
+    Console.WriteLine($"Sınıf: {group.ClassName}");
+    foreach (var student in group.Students)
+    {
+        Console.WriteLine($"  - Öğrenci: {student.StudentName}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ PratikLinqGroupJoin/README.md
@@ -0,0 +1,30 @@
+## Pratik - Linq Group Join
+-Bir okul veritabaný oluþturuyorsunuz ve aþaðýdaki iki tablonun verilerini kullanarak group join iþlemi gerçekleþtirmeniz gerekiyor:
+
+Öðrenciler Tablosu (Students)
+
+StudentId: Öðrencinin benzersiz kimliði
+
+StudentName: Öðrencinin adý
+
+ClassId: Öðrencinin ait olduðu sýnýfýn kimliði
+
+Sýnýflar Tablosu (Classes)
+
+ClassId: Sýnýfýn benzersiz kimliði
+
+ClassName: Sýnýfýn adý
+
+Aþaðýdaki görevleri yerine getirin:
+
+Tabloyu Tanýmlayýn:
+
+Student ve Class isimli iki sýnýf oluþturun. Her bir sýnýf için uygun veri türlerini kullanarak C# sýnýflarýný tanýmlayýn.
+
+Veri Listelemesi:
+
+
+LINQ Sorgusu:
+
+Öðrenciler ve sýnýflar arasýnda group join iþlemi yaparak, her sýnýfýn altýnda o sýnýfa ait olan öðrencilerin listelendiði bir sonuç elde edin. Sonuçlarý sýnýf adýyla birlikte, o sýnýfa ait öðrencilerin isimleriyle birlikte ekrana yazdýran bir LINQ sorgusu yazýn.
+
